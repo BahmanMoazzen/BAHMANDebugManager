465f530a4f0191b3194bdcfdac6dc052336c8420
Bahman Moazzen <bahmanmoazzen@gmail.com>
2025-09-20 15:51:53 +0930

    Version 1.0 Done!

---
+++ BAHMANDebugManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BAHMANDebugManager : MonoBehaviour
+{
+    public static BAHMANDebugManager _Instance;
+    Text _debugText;
+    [SerializeField] bool _enableDebug = true;
+    private void Awake()
+    {
+
+
+        if (_Instance == null)
+        {
+            _Instance = this;
+
+            _debugText = GetComponentInChildren<Text>();
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void _Log(string _text)
+    {
+        if (_enableDebug)
+        {
+            _debugText.text = _text;
+
+        }
+        Debug.Log(_text);
+    }
+}
